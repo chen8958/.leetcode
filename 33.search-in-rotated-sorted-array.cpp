cc9dcd631a3520351d1d2f800e41deff77960bb2
Tony Chen <yuxuan_chen@phison.com>
2024-12-27 19:21:13 +0800

    fix 33

--- 33.search-in-rotated-sorted-array.cpp
+++ 33.search-in-rotated-sorted-array.cpp
@@ -10,23 +10,25 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-#define GET_MIDDLE(r,l) (l == (r + l)/2)?(r):((r + l)/2)
 public:
     int search(vector<int>& nums, int target) {
+        if(nums.size() == 0){
+            return -1;
+        }
         int initial_number = nums[0];
-        int r = nums.size(), l = 0 ,middle;
+        int r = nums.size()-1, l = 0 ,middle;
         if(target > initial_number){
             // find right of 0
             while(r!=l){
-                middle = GET_MIDDLE(r,l);
+                middle = l + (r - l) / 2;
                 while(nums[middle] < initial_number){
-                    r = middle;
-                    middle = GET_MIDDLE(r,l);
+                    r = middle - 1;
+                    middle = l + (r - l) / 2;
                 }
                 if(nums[middle] > target){
-                    r = middle;
+                    r = middle - 1;
                 }else if(nums[middle] < target){
-                    l = middle;
+                    l = middle + 1;
                 }else{
                     return middle;
                 }
@@ -34,15 +36,15 @@ public:
         }else if(target < initial_number){
             // find left of end
             while(r!=l){
-                middle = GET_MIDDLE(r,l);
+                middle = l + (r - l) / 2;
                 while(nums[middle] > initial_number){
-                    l = middle;
-                    middle = GET_MIDDLE(r,l);
+                    l = middle + 1;
+                    middle = l + (r - l) / 2;
                 }
                 if(nums[middle] > target){
-                    r = middle;
+                    r = middle - 1;
                 }else if(nums[middle] < target){
-                    l = middle;
+                    l = middle + 1;
                 }else{
                     return middle;
                 }
