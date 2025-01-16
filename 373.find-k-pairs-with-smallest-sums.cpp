944549e05416908016a270cce010e78465b975aa
Tony Chen <yuxuan_chen@phison.com>
2025-01-16 14:24:26 +0800

    [heap][m][10] 373 add lambda function 1. more faster and general compare declaration. 2. lambda function type is decide by compiler, so it rely on auto and decltype()
    Fix test pattern:   na
    Impact Flash model: na

--- 373.find-k-pairs-with-smallest-sums.cpp
+++ 373.find-k-pairs-with-smallest-sums.cpp
@@ -11,57 +11,29 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-
-    struct node{
-        pair<int, int> idx;
-        // vector<int> nums;
-        int sum;
-        // bool operator()(const pair<int,int> l_p, const pair<int,int> r_p){
-        bool operator()(const node l_node, const node r_node){
-            if(l_node.sum > r_node.sum){
-                return true;
-            }else{
-                return false;
-            }
-        }
-        node(){};
-        node(const vector<int>& nums1, const vector<int>& nums2, int idx1, int idx2){
-            this->idx = make_pair(idx1, idx2);
-            // this->nums = {nums1[idx1], nums2[idx2]};
-            this->sum = nums1[idx1] + nums2[idx2];
-        };
-    };
-    typedef struct node NODE, *NODE_PTR;
-
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<vector<bool>> is_visted_map(nums1.size(), vector<bool>(nums2.size(),false));
-        // (num1, num2), sum
-        priority_queue<NODE, vector<NODE>, NODE> heap;
-        NODE current_node;
+        int x,y;
+        auto compare_lambda_function = [& nums1, & nums2](pair<int, int>p1, pair<int,int> p2){
+            return (nums1[p1.first]+nums2[p1.second]) > (nums1[p2.first] + nums2[p2.second]);
+        };
+        priority_queue<pair<int,int>,vector<pair<int,int>>, decltype(compare_lambda_function)> pq(compare_lambda_function);
+        int queue1_size = nums1.size();
+        int queue2_size = nums2.size();
         vector<vector<int>> ans;
-        heap.push(NODE(nums1,nums2,0,0));
-        is_visted_map[0][0] = true;
-        for(int i = 0; i < k ; i++){
-            current_node = heap.top();
-            ans.push_back({nums1[current_node.idx.first],nums2[current_node.idx.second]});
-            heap.pop();
-            // try to push new node to heap
-            // go right and down size
-            if(current_node.idx.first + 1 < nums1.size()){
-                if(is_visted_map[current_node.idx.first+1][current_node.idx.second] == false){
-                    is_visted_map[current_node.idx.first+1][current_node.idx.second] = true;
-                    heap.push(NODE(nums1, nums2, current_node.idx.first+1, current_node.idx.second));
-                }
-            }
-            if(current_node.idx.second + 1 < nums2.size()){
-                if(is_visted_map[current_node.idx.first][current_node.idx.second+1] == false){
-                    is_visted_map[current_node.idx.first][current_node.idx.second+1] = true;
-                    heap.push(NODE(nums1, nums2, current_node.idx.first, current_node.idx.second + 1));
-                }
+
+        for(int i = 0; i < min(queue1_size, k); i++){
+            pq.push(pair<int,int>{i,0});
+        }
+        while(k--){
+            x = pq.top().first;
+            y = pq.top().second;
+            ans.push_back({nums1[x] ,nums2[y]});
+            pq.pop();
+            if((y+1) < queue2_size){
+                pq.push(pair<int,int>{x, y+1});
             }
         }
-
         return ans;
     }
 };
